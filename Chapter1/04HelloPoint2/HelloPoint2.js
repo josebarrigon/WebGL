4a6ff09b09d866cfef493e67e03ccf08b285c8dd
Joseba Aitzol Larraza <j.aitzol@i3code.es>
2021-10-22 12:12:36 +0200

    Estableciendo atributos del vertex shader con javascript

--- Chapter1/04HelloPoint2/HelloPoint2.js
+++ Chapter1/04HelloPoint2/HelloPoint2.js
@@ -2,9 +2,10 @@
 // Vertex shader program
 var VSHADER_SOURCE =
 'attribute vec4 a_Position;\n' +
+'attribute float a_PointSize; \n' +
 'void main() {\n' +
 ' gl_Position = a_Position;\n' +
-' gl_PointSize = 10.0;\n' +
+' gl_PointSize = a_PointSize;\n' +
 '}\n';
 
 // Fragment shader program
@@ -30,12 +31,20 @@ return;
 //Get the storage location of attribute variable
 var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
 
+var a_PointSize = gl.getAttribLocation(gl.program,'a_PointSize');
+
 if(a_Position < 0 ){
     console.log("Failded to get storage a_position");
     return;
 }
+if(a_PointSize < 0 ){
+    console.log("Failded to get storage a_pointsize");
+    return;
+}
 // Pass vertex position to attribute variable
 gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+//Pass object size to attribute variable
+gl.vertexAttrib1f(a_PointSize, 5.0);
 // Set the color for clearing <canvas>
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 // Clear <canvas>
